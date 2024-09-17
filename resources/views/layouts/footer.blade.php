62bc95d976463e944c8c839db233b1045db0b396
libertus-libertus <libert.jobs@gmail.com>
2024-09-17 22:07:18 +0700

    Intagrate Admin LTE 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    Created by: Libertus
+  </div>
+  <strong>Copyright &copy; 2024 </strong> &mdash; All rights reserved.
+</footer>
